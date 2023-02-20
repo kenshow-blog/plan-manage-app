d0f0ddad1fe1c97e0a101386aa8ab09f9b2ea1eb
kenshow-blog <kensho02131@gmail.com>
2023-02-20 22:55:20 +0900

    フロント基盤作成

--- plan-manager-frontend/src/pages/_app.tsx
+++ plan-manager-frontend/src/pages/_app.tsx
@@ -2,12 +2,38 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "redux/store";
+import { useEffect } from "react";
+import { CacheProvider, EmotionCache, ThemeProvider } from "@emotion/react";
+import createEmotionCache from "createEmotionCache";
+import { createTheme, CssBaseline } from "@mui/material";
+const clientSideEmotionCache = createEmotionCache();
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        "Roboto",
+        '"Noto Sans JP"',
+        '"Helvetica"',
+        "Arial",
+        "sans-serif",
+      ].join(","),
+      fontSize: 14,
+    },
+  });
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
